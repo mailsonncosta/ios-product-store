f26a790e7991fc2c8d15b135119719ad0fc5b028
Mailson Costa <mailsonncosta@gmail.com>
2021-09-25 22:52:16 -0300

    Added the DetailsViewController

---
+++ DigioStore/DigioStore/Controllers/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  DigioStore
+//
+//  Created by Mailson Costa on 25/09/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
